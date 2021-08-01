eed071a7039caaa3ebee403ccb2ea35f782abe1b
Eric Wong <ericwong12@live.com>
2021-08-01 15:31:51 -0600

    Feat: HomePanel
    Boilerplate needs information and styles

---
+++ components/UI/Landing-Page/HomePanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <section class="panel">
+    <div class="contents">
+      <div v-for="content in contents" :key="content.name">
+        {{ content.name }}
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import { reactive } from "@nuxtjs/composition-api";
+export default {
+  data() {
+    return {
+      contents: [
+        {
+          name: "Project",
+        },
+        {
+          name: "Github",
+        },
+        {
+          name: "Contact",
+        },
+      ],
+    };
+  },
+  //   setup() {
+  //     const state = reactive({
+  //       contents: [{}],
+  //     });
+  //     return { state };
+  //   },
+};
+</script>
+
+<style>
+</style>
