8047daeaa739f8a0074eee87fd3144ba4435776e
Eric Wong <ericwong12@live.com>
2021-08-01 15:34:23 -0600

    Feat: default/layouts
    Layout setup for page, needs attention for mobile.

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,12 +1,46 @@
 <template>
-  <div>
-    <Nuxt />
+  <div class="page-container">
+    <div class="page-side-bar">
+      <Sidebar />
+    </div>
+    <div class="page-body">
+      <Navbar />
+      <Nuxt />
+      <Footer />
+    </div>
   </div>
 </template>
 
 <script>
-export default {};
+import Navbar from "~/components/UI/Navigation/Navbar.vue";
+import Footer from "~/components/UI/Navigation/Footer.vue";
+import Sidebar from "~/components/UI/Navigation/Sidebar.vue";
+export default {
+  components: {
+    Navbar,
+    Footer,
+    Sidebar,
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.page-container {
+  display: grid;
+  grid-template-columns: 10% 80% 10%;
+  grid-template-areas: "sidebar body .";
+}
+.page-body {
+  grid-area: body;
+}
+.page-side-bar {
+  display: flex;
+  align-items: center;
+  grid-area: sidebar;
+}
+@media only screen and (max-width: 880px) {
+  .page-side-bar {
+    display: none;
+  }
+}
 </style>
