7a875266c3b8360de9d91b04365fe41585d403b2
Eric Wong <ericwong12@live.com>
2021-08-02 16:18:38 -0600

    Feat: HomeContent
    Quick display of landing page options.
    Projects, about, and others

---
+++ components/UI/Landing-Page/HomeContent.vue
@@ -0,0 +1,48 @@
+<template>
+  <section class="pannels">
+    <div class="pannel" v-for="pannel in pannels" :key="pannel.name">
+      <img class="pannel-image" :src="pannel.image" :alt="pannel.alt" />
+      <span class="pannel-name">{{ pannel.name }}</span>
+    </div>
+  </section>
+</template>
+
+<script>
+import { reactive } from "@nuxtjs/composition-api";
+import projectsSVG from "~/components/UI/SVG/projects.svg";
+export default {
+  data() {
+    return {
+      pannels: [
+        {
+          name: "Project",
+          image: projectsSVG,
+          alt: "Website building drawing",
+        },
+        {
+          name: "Github",
+        },
+        {
+          name: "Contact",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.pannels {
+  background-color: lightgray;
+  height: 50vh;
+  display: flex;
+  justify-content: space-around;
+}
+.pannel {
+  display: inline-grid;
+}
+
+.pannel-image {
+  width: 50%;
+}
+</style>
