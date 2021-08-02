52805b8f244014cbb23325ec126614a79d6bb148
Eric Wong <ericwong12@live.com>
2021-08-02 16:19:17 -0600

    Feat: HomePanel
    Landing page's greetings.

--- components/UI/Landing-Page/HomePanel.vue
+++ components/UI/Landing-Page/HomePanel.vue
@@ -1,39 +1,45 @@
 <template>
-  <section class="panel">
-    <div class="contents">
-      <div v-for="content in contents" :key="content.name">
-        {{ content.name }}
+  <section class="container">
+    <div class="introduction">
+      <div class="introduction-text">
+        <span> Hi, how's it going? My name is Eric</span>
+        <HomeSkills />
       </div>
+      <img
+        class="introduction-image"
+        src="../SVG/guy-coding.svg"
+        alt="Guy coding drawing"
+      />
     </div>
+
+    <HomeContent />
   </section>
 </template>
 
 <script>
-import { reactive } from "@nuxtjs/composition-api";
+import HomeContent from "~/components/UI/Landing-Page/HomeContent.vue";
+import HomeSkills from "~/components/UI/Landing-Page/HomeSkills.vue";
 export default {
-  data() {
-    return {
-      contents: [
-        {
-          name: "Project",
-        },
-        {
-          name: "Github",
-        },
-        {
-          name: "Contact",
-        },
-      ],
-    };
+  components: {
+    HomeContent,
+    HomeSkills,
   },
-  //   setup() {
-  //     const state = reactive({
-  //       contents: [{}],
-  //     });
-  //     return { state };
-  //   },
 };
 </script>
 
-<style>
+<style scoped>
+.introduction {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+}
+.introduction-text {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  font-size: 2rem;
+}
+
+.introduction-image {
+  width: 100%;
+}
 </style>
