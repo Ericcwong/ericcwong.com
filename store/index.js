23998306406d9bf6b34a4e7eeb1c0c7347fec7fa
Eric Wong <ericwong12@live.com>
2021-08-03 16:08:00 -0600

    Feat: Vuex state
    Add more information to projects

--- store/index.js
+++ store/index.js
@@ -4,8 +4,13 @@ export const state = () => ({
       name: "Crypto Market",
       image:
         "https://lh3.googleusercontent.com/Lk_5dJKpKsSY32kyQhI3ZVHdNs9vlbNUkPYHLguQXFkyFPjXiPByoBH9fZjDWUSve7zDjmP1T0FNHCzd1ZbxTB7NmQQXHjSlZiZUf3_GiZhrkVGO3KCpESLyZb1f-Vzrky6SrhHRDA=w2400",
-      shortDescription: "",
-      description: "",
+      frontend: "",
+      backend: "",
+      shortDescription: "Keep up with the prices of cryptocurrencies!",
+      description:
+        "Crypto Market retrieves data from Coingecko's API and displays cryptocurrencies by their market cap.",
+      thingsILearned:
+        "How to retrieve data without slowing down the website, Chartjs for display graphs, and improvement to the design aspect. The design is not exactly what I would want but its an improvement.",
       deployLink:
         "https://ericcwong-crypto-market.herokuapp.com/pancakeswap-token",
       githubLink: "https://github.com/Ericcwong/Crypto-Market",
@@ -13,7 +18,7 @@ export const state = () => ({
     {
       name: "Blog",
       image:
-        "https://lh3.googleusercontent.com/Lk_5dJKpKsSY32kyQhI3ZVHdNs9vlbNUkPYHLguQXFkyFPjXiPByoBH9fZjDWUSve7zDjmP1T0FNHCzd1ZbxTB7NmQQXHjSlZiZUf3_GiZhrkVGO3KCpESLyZb1f-Vzrky6SrhHRDA=w2400",
+        "https://lh3.googleusercontent.com/RLV45PA7LpTpm2kTCDGWyBhPGMk7bqcvcfbhv-pXF4ZUayExpUH-Y4N1PiOa1q2fDHJ48EsxujMaJDbRXnVRb9SA_q_hgI3d3IGyjBJxXC7DRh8nQ174t7Up8zUSwbuSzqcp2528uQ=w2400",
       shortDescription: "",
       description: "",
       deployLink: "https://ericcwong-blog.herokuapp.com/",
@@ -21,7 +26,8 @@ export const state = () => ({
     },
     {
       name: "Mkcapped",
-      image: "",
+      image:
+        "https://lh3.googleusercontent.com/5-bkfp8gsI1xA9IjL3cW3tLhcNCEp2jPMY37Ihnwgt2uiwsYp-ugSfhWS41PIG6RJdda6fzQEa_GQH0r5EZ_jRNQf_gJ8U8V-QHk0hppa65zmCWXZhf_ul3hOB1jtMFZZOgJIc5G2g=w2400",
       shortDescription: "",
       description: "",
       deployLink: "",
