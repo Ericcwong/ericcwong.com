675d98066c9e23f38fa8a6f4704dd673e2d783e8
Eric Wong <ericwong12@live.com>
2021-08-03 13:14:44 -0600

    Feat: Vuex state

---
+++ store/index.js
@@ -0,0 +1,31 @@
+export const state = () => ({
+  projects: [
+    {
+      name: "Crypto Market",
+      image:
+        "https://lh3.googleusercontent.com/Lk_5dJKpKsSY32kyQhI3ZVHdNs9vlbNUkPYHLguQXFkyFPjXiPByoBH9fZjDWUSve7zDjmP1T0FNHCzd1ZbxTB7NmQQXHjSlZiZUf3_GiZhrkVGO3KCpESLyZb1f-Vzrky6SrhHRDA=w2400",
+      shortDescription: "",
+      description: "",
+      deployLink:
+        "https://ericcwong-crypto-market.herokuapp.com/pancakeswap-token",
+      githubLink: "https://github.com/Ericcwong/Crypto-Market",
+    },
+    {
+      name: "Blog",
+      image:
+        "https://lh3.googleusercontent.com/Lk_5dJKpKsSY32kyQhI3ZVHdNs9vlbNUkPYHLguQXFkyFPjXiPByoBH9fZjDWUSve7zDjmP1T0FNHCzd1ZbxTB7NmQQXHjSlZiZUf3_GiZhrkVGO3KCpESLyZb1f-Vzrky6SrhHRDA=w2400",
+      shortDescription: "",
+      description: "",
+      deployLink: "https://ericcwong-blog.herokuapp.com/",
+      githubLink: "https://github.com/Ericcwong/Blog",
+    },
+    {
+      name: "Mkcapped",
+      image: "",
+      shortDescription: "",
+      description: "",
+      deployLink: "",
+      githubLink: "",
+    },
+  ],
+});
