25a5a4ad7b8678eaa3623892718d5ac06907fe00
Eric Wong <ericwong12@live.com>
2021-08-03 13:14:31 -0600

    Fix: HomeContent & Projects
    Reuseable information is moved to Vuex state.

--- components/UI/Landing-Page/HomeContent.vue
+++ components/UI/Landing-Page/HomeContent.vue
@@ -32,38 +32,14 @@
 
 <script>
 export default {
+  computed: {
+    projects() {
+      return this.$store.state.projects;
+    },
+  },
   data() {
     return {
       show: false,
-      projects: [
-        {
-          name: "Crypto Market",
-          image:
-            "https://lh3.googleusercontent.com/Lk_5dJKpKsSY32kyQhI3ZVHdNs9vlbNUkPYHLguQXFkyFPjXiPByoBH9fZjDWUSve7zDjmP1T0FNHCzd1ZbxTB7NmQQXHjSlZiZUf3_GiZhrkVGO3KCpESLyZb1f-Vzrky6SrhHRDA=w2400",
-          shortDescription: "",
-          description: "",
-          deployLink:
-            "https://ericcwong-crypto-market.herokuapp.com/pancakeswap-token",
-          githubLink: "https://github.com/Ericcwong/Crypto-Market",
-        },
-        {
-          name: "Blog",
-          image:
-            "https://lh3.googleusercontent.com/Lk_5dJKpKsSY32kyQhI3ZVHdNs9vlbNUkPYHLguQXFkyFPjXiPByoBH9fZjDWUSve7zDjmP1T0FNHCzd1ZbxTB7NmQQXHjSlZiZUf3_GiZhrkVGO3KCpESLyZb1f-Vzrky6SrhHRDA=w2400",
-          shortDescription: "",
-          description: "",
-          deployLink: "https://ericcwong-blog.herokuapp.com/",
-          githubLink: "https://github.com/Ericcwong/Blog",
-        },
-        {
-          name: "Mkcapped",
-          image: "",
-          shortDescription: "",
-          description: "",
-          deployLink: "",
-          githubLink: "",
-        },
-      ],
     };
   },
 };
--- pages/projects.vue
+++ pages/projects.vue
@@ -12,26 +12,13 @@
 
 <script>
 export default {
+  computed: {
+    projects() {
+      return this.$store.state.projects;
+    },
+  },
   data() {
-    return {
-      projects: [
-        {
-          name: "Crypto Market",
-          image:
-            "https://lh3.googleusercontent.com/Lk_5dJKpKsSY32kyQhI3ZVHdNs9vlbNUkPYHLguQXFkyFPjXiPByoBH9fZjDWUSve7zDjmP1T0FNHCzd1ZbxTB7NmQQXHjSlZiZUf3_GiZhrkVGO3KCpESLyZb1f-Vzrky6SrhHRDA=w2400",
-          description: "",
-          deployLink:
-            "https://ericcwong-crypto-market.herokuapp.com/pancakeswap-token",
-          githubLink: "https://github.com/Ericcwong/Crypto-Market",
-        },
-        {
-          name: "Blog",
-          image: "",
-          deployLink: "https://ericcwong-blog.herokuapp.com/",
-          githubLink: "https://github.com/Ericcwong/Blog",
-        },
-      ],
-    };
+    return {};
   },
 };
 </script>
