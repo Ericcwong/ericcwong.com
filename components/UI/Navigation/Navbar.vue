57c633a2c8ee6ecc83739ebfc6c1bc13f1db569e
Eric Wong <ericwong12@live.com>
2021-09-12 01:37:52 -0600

    Feat: Navbar
    Boilerplate
    Needs style for mobile

--- components/UI/Navigation/Navbar.vue
+++ components/UI/Navigation/Navbar.vue
@@ -1,7 +1,60 @@
-<template></template>
+<template>
+  <nav>
+    <ul class="nav-left">
+      <li><nuxt-link to="/" class="nav-title">Eric Wong</nuxt-link></li>
+    </ul>
+    <ul class="nav-center"></ul>
+    <ul class="nav-right">
+      <li v-for="navLink in navLinks" :key="navLink.name">
+        <nuxt-link :to="navLink.to">{{ navLink.name }}</nuxt-link>
+      </li>
+    </ul>
+  </nav>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      navLinks: [
+        { name: "Home", to: "/" },
+        { name: "Project", to: "/project" },
+        { name: "Contact", to: "/contact" },
+      ],
+    };
+  },
+};
 </script>
 
-<style></style>
+<style scoped>
+nav {
+  margin-top: 1.25rem;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.nav-left {
+  flex: 1;
+
+  text-align: center;
+}
+.nav-center {
+  flex: 4 1 0;
+}
+.nav-right {
+  display: flex;
+  flex: 2;
+  justify-content: space-evenly;
+}
+.nav-title {
+  font-size: 2rem;
+}
+li {
+  color: white;
+  list-style: none;
+}
+a {
+  text-decoration: none;
+  color: white;
+}
+</style>
