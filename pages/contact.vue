a893d5582f67df25993e3c418fe20af61f7349f5
Eric Wong <ericwong12@live.com>
2021-09-12 01:38:42 -0600

    Feat: Contact.vue
    Boilerplate

---
+++ pages/contact.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="container">
+    <form>
+      <label for="email">Email</label>
+      <input type="email" id="email" v-model="email" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  background-color: #ffffff;
+  background-image: linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+input {
+  border: 1px solid black;
+  border-radius: 1.5rem;
+}
+</style>
