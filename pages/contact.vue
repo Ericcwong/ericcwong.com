c59cb5d0d49eb63fd38aa6c527a79a9e82f2e6a8
Eric Wong <ericwong12@live.com>
2021-09-29 14:21:45 -0600

    Chore:  contact and index (pages) Remove unused code

--- pages/contact.vue
+++ pages/contact.vue
@@ -13,8 +13,6 @@ export default {};
 
 <style scoped>
 .container {
-  background-color: #ffffff;
-  background-image: linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%);
   display: flex;
   justify-content: center;
   align-items: center;
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="container">
     <div>
       <HomePanel />
     </div>
@@ -20,5 +20,4 @@ export default {
 };
 </script>
 
-<style>
-</style>
+<style scoped></style>
